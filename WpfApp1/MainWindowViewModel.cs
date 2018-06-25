17347dc81a38f86b9db6b1a46344698f5071cbcf
Simpu <swarajksantra@gmail.com>
2018-06-24 21:49:11 -0700

    First commit

---
+++ WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public MainWindowViewModel()
+        {
+            Persons.Add(new Person { ID = 1, Name = "Simpu", Email = "Simpu@gmail.com" });
+            Persons.Add(new Person { ID = 2, Name = "Subhajit", Email = "Basak@gmail.com" });
+            Persons.Add(new Person { ID = 3, Name = "Anjan", Email = "Ghosh@gmail.com" });
+            Persons.Add(new Person { ID = 4, Name = "Swaraj", Email = "Ketan@gmail.com" });
+            Persons.Add(new Person { ID = 4, Name = "Monika", Email = "Sharma@gmail.com" });
+        }
+
+        private ObservableCollection<Person> _Persons;
+
+        public ObservableCollection<Person> Persons
+        {
+            get
+            {
+                if (_Persons == null)
+                    _Persons = new ObservableCollection<Person>();
+                return _Persons;
+            }
+            set
+            {
+                _Persons = value;
+                OnPropertyChanged(nameof(Persons));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ WpfApp1/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private int _ID;
+        private string _Name;
+        private string _Email;
+
+        public int ID
+        {
+            get => _ID;
+            set
+            {
+                _ID = value;
+                OnPropertyChanged(nameof(ID));
+            }
+        }
+        public string Name
+        {
+            get => _Name; set
+            {
+                _Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        public string Email
+        {
+            get => _Email; set
+            {
+                _Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
